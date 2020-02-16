e349fe2b30e34633d3059d9235c57b37ecb8aa23
Wen Chia Yang <wcyang1@uci.edu>
2020-02-16 02:44:48 -0800

    (add) api and models

---
+++ app/src/main/java/edu/uci/thanote/apis/joke/Joke.java
@@ -0,0 +1,57 @@
+package edu.uci.thanote.apis.joke;
+
+public class Joke {
+    private int id;
+    private String category;
+    private String type;
+    private boolean error;
+    private Flag flags;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public boolean isError() {
+        return error;
+    }
+
+    public Flag getFlags() {
+        return flags;
+    }
+
+    class Flag {
+        private boolean nsfw;
+        private boolean religious;
+        private boolean political;
+        private boolean racist;
+        private boolean sexist;
+
+        public boolean isNsfw() {
+            return nsfw;
+        }
+
+        public boolean isReligious() {
+            return religious;
+        }
+
+        public boolean isPolitical() {
+            return political;
+        }
+
+        public boolean isRacist() {
+            return racist;
+        }
+
+        public boolean isSexist() {
+            return sexist;
+        }
+    }
+}
---
+++ app/src/main/java/edu/uci/thanote/apis/joke/SingleJoke.java
@@ -0,0 +1,11 @@
+package edu.uci.thanote.apis.joke;
+
+import java.util.List;
+
+public class SingleJoke extends Joke {
+    private String joke;
+
+    public String getJoke() {
+        return joke;
+    }
+}
