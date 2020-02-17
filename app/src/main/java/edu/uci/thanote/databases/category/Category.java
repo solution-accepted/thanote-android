9f5b36334882159199015d60b0f5d3bbb8c208e1
Wen Chia Yang <wcyang1@uci.edu>
2020-02-17 01:36:40 -0800

    (add) documentation of database tables

--- app/src/main/java/edu/uci/thanote/databases/category/Category.java
+++ app/src/main/java/edu/uci/thanote/databases/category/Category.java
@@ -4,8 +4,19 @@ import androidx.room.Entity;
 import androidx.room.Index;
 import edu.uci.thanote.databases.general.BaseTable;
 
+/**
+ * Category is a table that contains category fields
+ * <p><h3>When initiating the database, the default category will be auto-created with id {@link Category#DEFAULT_CATEGORY_ID} and name {@link Category#DEFAULT_CATEGORY_NAME}</h3></p>
+ * <p>1. id: Primary Key, auto increased</p>
+ * <p>2. createDate: Date when this object is created</p>
+ * <p>3. name: category's name, Unique</p>
+ * @see BaseTable
+ */
 @Entity(tableName = "category_table", indices = {@Index(value = "name", unique = true)})
 public class Category extends BaseTable {
+    public static final int DEFAULT_CATEGORY_ID = 1;
+    public static final String DEFAULT_CATEGORY_NAME = "default";
+
     // Column Info
     private String name;
 
--- app/src/main/java/edu/uci/thanote/databases/note/Note.java
+++ app/src/main/java/edu/uci/thanote/databases/note/Note.java
@@ -1,8 +1,19 @@
 package edu.uci.thanote.databases.note;
 
 import androidx.room.Entity;
+import edu.uci.thanote.databases.category.Category;
 import edu.uci.thanote.databases.general.BaseTable;
 
+/**
+ * Note is a table that contains note fields
+ * <p>1. id: Primary Key, auto increased</p>
+ * <p>2. createDate: Date when this object is created</p>
+ * <p>3. title: note's title, not null (should set empty is no value)</p>
+ * <p>4. detail: note's detail, not null (should set empty is no value)</p>
+ * <p>5. categoryId: mapping note with category by using category id (default = {@link Category#DEFAULT_CATEGORY_ID})</p>
+ * <p>6. imageUrl: note's image url, not null (should set empty is no value)</p>
+ * @see BaseTable
+ */
 @Entity(tableName = "note_table")
 public class Note extends BaseTable {
     // Column Infos
