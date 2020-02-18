b7929593e7c074af7a161b14926ab1fa62ffb093
Wen Chia Yang <wcyang1@uci.edu>
2020-02-17 19:24:15 -0800

    (modify) fix api query bug

--- app/src/main/java/edu/uci/thanote/apis/joke/JokeAPIInterface.java
+++ app/src/main/java/edu/uci/thanote/apis/joke/JokeAPIInterface.java
@@ -11,9 +11,9 @@ public interface JokeAPIInterface {
     @GET("Any?type=single")
     Call<SingleJoke> getSingleJoke();
 
-    @GET("Any?")
-    Call<SingleJoke> getSingleJokeBy(@Query("type=single&contains") String key);
+    @GET("Any?type=single")
+    Call<SingleJoke> getSingleJokeBy(@Query("contains") String key);
 
-    @GET("Any?")
-    Call<TwoPartJoke> getTwoPartJokeBy(@Query("type=twopart&contains") String key);
+    @GET("Any?type=twopart")
+    Call<TwoPartJoke> getTwoPartJokeBy(@Query("contains") String key);
 }
