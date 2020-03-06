bf7532342237f964c0d511e1d018b69480fcfbd1
Joxon <cijophxon@gmail.com>
2020-03-06 13:46:27 -0800

    {modify) testing library dependencies

--- app/src/androidTest/java/edu/uci/thanote/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/edu/uci/thanote/ExampleInstrumentedTest.java
@@ -2,7 +2,7 @@ package edu.uci.thanote;
 
 import android.content.Context;
 import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.runner.AndroidJUnit4;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
